ef1de6db52db00390a2de97877a36a11ac674505
Mohammad Natour <mohammad.alnatour@hotmail.com>
2019-10-04 01:17:22 +0300

    add two funtions max

--- main.c
+++ main.c
@@ -7,3 +7,16 @@ int main()
     printf("Abed");
     return 0;
 }
+int max(int x,int y)
+{
+    if(x>y)
+        return x;
+    return y;
+}
+void printMax(int x, int y)
+{
+    if(x>y)
+        printf("%d",x);
+    else
+        printf("%d",y);
+}
